13aa887f34a77ff08099aaba7e9271e508fea89a
FranAcosta07 <acostafranmarce@gmail.com>
2024-09-01 17:30:08 -0300

    agrega desafio2 al final del archivo

--- tp_07_objetos/proyecto/app.js
+++ tp_07_objetos/proyecto/app.js
@@ -1,6 +1,7 @@
 // app.js
 
 // Importar el módulo de películas
+const { isUtf8 } = require('buffer');
 const peliculas = require('./peliculas');
 
 // Mostrar el listado de películas
@@ -13,4 +14,18 @@ peliculas.forEach(pelicula => {
     console.log(`Precio: $${pelicula.price}`);
     console.log(`Género: ${pelicula.genre}`);
     console.log('--------------------------');
+});
+//aca comienza desafio2
+// app.js
+
+const fs = require('fs'); // Importar el módulo 'fs' para manejar archivos
+const path = './mensaje.txt'; // Ruta del archivo mensaje.txt
+
+// Leer el contenido del archivo
+fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error al leer el archivo:', err);
+        return;
+    }
+    console.log(`Contenido del archivo: ${data}`);
 });
