f5186607913d8ab90a24b167ae85580ce0019277
FranAcosta07 <acostafranmarce@gmail.com>
2024-09-01 13:27:52 -0300

    agrega segundo desafio

---
+++ tp_04_funciones/clase4Desafio2.js
@@ -0,0 +1,14 @@
+let tipoDeHamburguesa = tipoDeHamburguesaCargado.toLowerCase();
+let tipoDeHamburguesaCargado = "carne";
+
+function costoTotal(nombreUsuario, apellidoUsuario, tipoDeHamburguesa, a, b, c, d, e, f, g, h, i) {
+    let precioBase =
+        tipoDeHamburguesa = "carne" ? 1800 :
+            tipoDeHamburguesa === "pollo" ? 1500 :
+                tipoDeHamburguesa === "vegetariana" ? 1200 : a = 0;
+}
+if (precioBase === 0) {
+    return "Por favor elija un tipo de hamburguesa para realizar su orden";
+}
+let total = precioBase;
+total += b;
