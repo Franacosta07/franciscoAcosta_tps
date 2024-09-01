920a2777f6d267199b4896b717351f58586efa04
FranAcosta07 <acostafranmarce@gmail.com>
2024-09-01 13:25:11 -0300

    agrega tercer desafio

---
+++ tp_02_variables/clase2Desafio3.js
@@ -0,0 +1,20 @@
+/* declarar dos variables, otorgando el nombre y los valoresnuméricos que consideres. Ejemplo: let numero1 = 16;3. Usando el console.log() mostrar al usuario los resultados de las siguientes operacionesaritméticas, en función de los valores asignados a las variables:a. sumab. restac. multiplicaciónd. divisióne. módulo4. ¿Cual de los valores es mayor? Si el valor es mayor, mostrará un valor boolean true. Delo contrario mostrará false5. ¿Cual de los valores es menor? Si el valor es menor mostrará un valor boolean true. Delo contrario mostrará false*/
+let  numero1 = 16;
+let  numero2 = 8;
+
+let suma =  numero1 + numero2;
+let resta =   numero1 - numero2;
+let multiplica = numero1 *  numero2;
+let divide =  numero1 /  numero2;
+let  modulo =  numero1 %  numero2;
+let esMayor =  numero1 > numero2;
+let esMenor =   numero1 < numero2;
+
+console.log(suma);
+console.log(resta);
+console.log(multiplica);
+console.log(divide);
+console.log(modulo);
+console.log(esMayor);
+console.log(esMenor);
+
