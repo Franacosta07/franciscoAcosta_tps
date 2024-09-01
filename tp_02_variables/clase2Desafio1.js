7309943be4e77304135e28fad2baa0df3518af79
FranAcosta07 <acostafranmarce@gmail.com>
2024-09-01 13:23:35 -0300

    agrega primer desafio

---
+++ tp_02_variables/clase2Desafio1.js
@@ -0,0 +1,29 @@
+let nombre = "pedrito"
+let apellido = "pereyra"
+let edad = 25
+let numeroDeTeléfono = 342342331
+let esSocio = true
+let FechaDelPartido =  "2022-01-01"
+let horaDelPartido = "15:10"
+let NúmeroDeSilla = "m7"
+let PrecioDelBoleto =  15000
+
+/* Usando el console.log(), muestra al usuario el valor asignado a cada variable. Finalmente, utilizando el console.log() y el typeof, muestra al usuario el tipo de dato decada una de las variables utilizadas*/
+
+console.log(nombre +" "+ apellido);
+console.log(edad);
+console.log(numeroDeTeléfono);
+console.log(esSocio);
+console.log(FechaDelPartido);
+console.log(horaDelPartido);
+console.log(NúmeroDeSilla);
+console.log(PrecioDelBoleto);
+console.log(typeof nombre);
+console.log(typeof apellido);
+console.log(typeof edad);
+console.log(typeof numeroDeTeléfono);
+console.log(typeof esSocio);
+console.log(typeof FechaDelPartido);
+console.log(typeof horaDelPartido);
+console.log(typeof NúmeroDeSilla);
+console.log(typeof PrecioDelBoleto);
