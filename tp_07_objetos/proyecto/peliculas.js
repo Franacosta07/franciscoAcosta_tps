de1773057894e52453dbbfd3e214d27e33f9dd80
FranAcosta07 <acostafranmarce@gmail.com>
2024-09-01 17:17:26 -0300

    agrega array de objetos

---
+++ tp_07_objetos/proyecto/peliculas.js
@@ -0,0 +1,45 @@
+const peliculas = [
+    {
+        id: 1,
+        rating: 8.5,
+        awards: 5,
+        length: 120,
+        price: 10.99,
+        genre: 'Acción'
+    },
+    {
+        id: 2,
+        rating: 7.2,
+        awards: 2,
+        length: 90,
+        price: 8.99,
+        genre: 'Animación'
+    },
+    {
+        id: 3,
+        rating: 9.0,
+        awards: 10,
+        length: 150,
+        price: 12.99,
+        genre: 'Aventuras'
+    },
+    {
+        id: 4,
+        rating: 6.8,
+        awards: 1,
+        length: 110,
+        price: 9.99,
+        genre: 'Comedia'
+    },
+    {
+        id: 5,
+        rating: 7.5,
+        awards: 3,
+        length: 105,
+        price: 11.99,
+        genre: 'Suspenso'
+    }
+];
+
+// Exportar el array de películas
+module.exports = peliculas;
