94460107af3a83f5d7c3253268e6cd33c7f23617
FranAcosta07 <acostafranmarce@gmail.com>
2024-09-09 14:10:46 -0300

    finaliza paso 2 y agrega estilos (hecho en puerta18)

--- tp_11_estructura/app.js
+++ tp_11_estructura/app.js
@@ -1,17 +1,14 @@
 const path = require("path");
 const express = require("express");
-const { dir } = require("console");
 const app = express();
-const port = 3030;
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, "public"))); 
+app.use(express.static(path.join(__dirname, "public")));
 
-//consola
+app.get('/', (req, res) => {
+    return res.sendFile(path.join(__dirname, 'views', 'home.html'))
+});
+
 app.listen(port, () => {
-    console.log(`Server running in http://localhost:${port}`);
-  });
+    console.log(`Server runung in http://localhost:${port}`)});
 
-//RUTAS
-app.get("/", (req, res) => {
-    return res.sendFile(path.join(__dirname, "views", "home.html"));
-  })
